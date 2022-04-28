5728c1c6b3436ae5e9000f612aff88449103847e
Destructa321 <amey.damle@mail.utoronto.ca>
2022-04-27 21:41:46 -0400

    Added default constructor for Employee

--- src/main/java/tech/ameydamle/employeemanager/model/Employee.java
+++ src/main/java/tech/ameydamle/employeemanager/model/Employee.java
@@ -17,6 +17,8 @@ public class Employee implements Serializable {
     @Column(nullable = false, updatable = false) //Cannot be null, cannot be updated
     private String employeeCode;
 
+    public Employee() { }
+
     public Employee(String name, String email, String jobTitle, String phone, String imageUrl, String employeeCode) {
         this.name = name;
         this.email = email;
