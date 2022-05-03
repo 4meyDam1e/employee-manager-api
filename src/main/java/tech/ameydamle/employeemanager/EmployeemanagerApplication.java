def0904b8cb154a74395425f7bee983fd850ca62
Destructa321 <amey.damle@mail.utoronto.ca>
2022-05-03 13:16:33 -0400

    Added Heroku website as an allowed origin

--- src/main/java/tech/ameydamle/employeemanager/EmployeemanagerApplication.java
+++ src/main/java/tech/ameydamle/employeemanager/EmployeemanagerApplication.java
@@ -16,12 +16,12 @@ public class EmployeemanagerApplication {
 		SpringApplication.run(EmployeemanagerApplication.class, args);
 	}
 
-	//Allow the frontend Angular app to use this backend Spring Boot app.
+	//Allow the (LOCALLY HOSTED) frontend Angular app to use this backend Spring Boot app.
 	@Bean
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://https://spring-employee-manager-app.herokuapp.com"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
